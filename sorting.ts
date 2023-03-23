11fe8b5ed028139d980d5a0192a29bb9838b3f17
Olivia Grace <121794969+olivia-clarkeedwards@users.noreply.github.com>
2023-03-23 18:39:55 +1300

    create fillArr function to create test array

---
+++ sorting.ts
@@ -0,0 +1,24 @@
+// Testing for sorting algorithmnpm i
+
+const testGroup = [
+  'John',
+  'Mary',
+  'Dhan-nun',
+  'Aurora',
+  'Rawiri',
+  'Ben',
+  'Alice',
+  'Lily',
+  'Jenny',
+  'Ken',
+]
+
+function fillArray() {
+  const numTestArr = []
+  for (let i = 1; i <= 15; i++) {
+    numTestArr.push(i)
+  }
+  return numTestArr
+}
+
+console.log(fillArray())
