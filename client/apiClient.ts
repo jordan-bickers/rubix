6710cbaa276d1c6d61c326450d3972ba71424e34
Gerard Verhaart <121985303+gerard-verhaart@users.noreply.github.com>
2023-03-24 11:14:47 +1300

    added external api function

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,10 +1,20 @@
 import request from 'superagent'
-import { Member } from '../common/member'
+import { response } from 'express'
+import { Member, ExternalMember } from '../common/member'
+
+const serverURL = 'http://localhost:3000/api/v1'
+const externalURL = 'https://randomuser.me/api/'
 
 export function getGreeting(): Promise<string> {
   return request.get('/greeting').then((res) => res.body.greeting)
 }
 
 export function getAllMembers(): Promise<Member[]> {
-  return request.get('/').then((res) => res.body)
+  return request.get(`${serverURL}`).then((res) => res.body)
+}
+
+export function getRandomName(): Promise<ExternalMember> {
+  return request
+    .get(`${externalURL}`)
+    .then((response) => response.body.results[0].name)
 }
