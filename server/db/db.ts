242a3bfba628913fb9b3dfb34ac023630c5eef6d
Olivia Grace <121794969+olivia-clarkeedwards@users.noreply.github.com>
2023-03-24 10:01:11 +1300

    added first api client route

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,5 +1,4 @@
 import connection from './connection'
-// Routes + Interfaces go here
 import { Member } from '../../common/member'
 
 export function getAllMembers(db = connection): Promise<Member[]> {
